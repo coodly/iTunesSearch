72f3a77e583a460e1abb79a27319b69e1fbb0062
Jaanus Siim <jaanus@jaanussiim.com>
2016-04-30 19:03:27 +0300

    Add sample project

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Jaanus Siim on 30/04/16.
+//  Copyright © 2016 Coodly LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
